cdafc86dbbe6d741fa5f6ff384fcb9d1739fc78f
fa7e <111585302+fa7e@users.noreply.github.com>
2025-02-07 15:39:35 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Ruta principal
+@app.route("/")
+def index():
+    return render_template("index.html")  # Renderiza index.html desde templates
+
+# Ejecutar el servidor
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
